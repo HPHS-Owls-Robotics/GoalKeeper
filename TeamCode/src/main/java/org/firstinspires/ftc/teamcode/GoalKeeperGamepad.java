98476bcfbaac3453d6bf2e390547d4d7a154093b
adelineec <148278400+adelineec@users.noreply.github.com>
2024-04-30 16:42:24 -0400

    Update GoalKeeperGamepad.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoalKeeperGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoalKeeperGamepad.java
@@ -2,11 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="GoalKeeperGamepad", group="12417")
 
